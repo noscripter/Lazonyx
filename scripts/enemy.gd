7e1f0dc7fbf6325f840539de33269e003075c95e
Ted Williams <tedwilliams1@gmail.com>
2017-11-08 13:17:44 +0000

    added basic enemy

---
+++ scripts/enemy.gd
@@ -0,0 +1,36 @@
+extends RigidBody2D
+
+export var move_speed = Vector2(150, 0)
+export var max_horizontal_speed = 200
+export var max_vertical_speed = 450
+
+var body = self
+
+
+func _ready():
+	set_fixed_process(true)
+	pass
+	
+func _fixed_process(delta):
+	addxvel(max
+	pass
+	
+# add x velocity
+func addxvel(amount):
+	var current_vel = body.get_linear_velocity()
+	var current_y_vel = current_vel.y
+	var new_vel = current_vel + Vector2(amount,0)
+	new_vel.x = clamp(new_vel.x,-max_horizontal_speed, max_horizontal_speed)
+	body.set_linear_velocity(new_vel)
+
+# add y velocity
+func addyvel(amount):
+	var current_vel = body.get_linear_velocity()
+	var current_x_vel = current_vel.x
+	var new_vel = current_vel + Vector2(0,amount)
+	new_vel.y = clamp(new_vel.y,-max_vertical_speed, max_vertical_speed)
+	body.set_linear_velocity(new_vel)
+	
+# set x velocity
+func setxvel(amount):
+	body.set_linear_velocity(Vector2(amount,body.get_linear_velocity().y))
--- scripts/player.gd
+++ scripts/player.gd
@@ -25,18 +25,20 @@ func _fixed_process(delta):
 	if Input.is_action_pressed("jump"):
 		var colliding_bodies = body.get_colliding_bodies()
 		for body in colliding_bodies:
-			if body.is_in_group("platforms"):
+			if body.is_in_group("jump_surface"):
 				addyvel(-max_vertical_velocity)
 	
 	pass
-	
+
+# add x velocity
 func addxvel(amount):
 	var current_vel = body.get_linear_velocity()
 	var current_y_vel = current_vel.y
 	var new_vel = current_vel + Vector2(amount,0)
 	new_vel.x = clamp(new_vel.x,-max_horizontal_velocity, max_horizontal_velocity)
 	body.set_linear_velocity(new_vel)
-	
+
+# add y velocity
 func addyvel(amount):
 	var current_vel = body.get_linear_velocity()
 	var current_x_vel = current_vel.x
@@ -44,5 +46,6 @@ func addyvel(amount):
 	new_vel.y = clamp(new_vel.y,-max_vertical_velocity, max_vertical_velocity)
 	body.set_linear_velocity(new_vel)
 	
+# set x velocity
 func setxvel(amount):
 	body.set_linear_velocity(Vector2(amount,body.get_linear_velocity().y))
