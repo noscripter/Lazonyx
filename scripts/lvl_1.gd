7de6f2fa2bdbd87a9f82ed119630642f4f773c3b
Ted Williams <tedwilliams1@gmail.com>
2017-11-21 13:12:38 +0000

    cleaned up 1v1 main script code and added comments

--- scripts/lvl_1.gd
+++ scripts/lvl_1.gd
@@ -1,6 +1,6 @@
 extends Node
 
-#children
+# get references to children
 onready var enemy_spawner  = get_node("enemy_spawner")
 onready var player         = get_node("player")
 onready var debug_circle   = get_node("debug_circle")
@@ -8,7 +8,8 @@ onready var menu_settings  = get_node("menu_settings")
 onready var menu_game_over = get_node("menu_game_over")
 onready var hud            = get_node("hud")
 
-var laser_ignore_objects =[player]
+# array of objects for laser to ignore
+var laser_ignore_objects = [player]
 
 #game
 var start_time = OS.get_unix_time()
@@ -21,40 +22,50 @@ var number_of_enemies = 0
 var max_number_of_enemies = 1
 var time_between_enemy_spawns = 1.0
 
-
-
 #player
 var current_score   = 0
 var top_score       = 0
 var points_per_goal = 1
 
 func _ready():
+	# connect events to functions
 	player.connect("entered_goal", self, "_player_entered_goal")
 	player.connect("fired_shot", self, "_player_fired_shot")
+	menu_game_over.connect("btn_menu_main_pressed", self, "_menu_game_over_btn_menu_main_pressed")
+	
+	# hide menus
 	menu_settings. hide()
 	menu_game_over.hide()
-	menu_game_over.connect("btn_menu_main_pressed", self, "_menu_game_over_btn_menu_main_pressed")
+	
+	# load top score from savegame
 	top_score = file_manager.load_top_score()
+	
+	# initialise HUD
 	hud.set_top_score(top_score)
 	hud.set_current_score(current_score)
 	hud.set_ammo(player.ammo)
 	hud.set_lives(player.lives)
 	hud.show()
+	
 	set_process(true)
 	pass
 
 func _process(delta):
 	# calculate game time
 	game_time = OS.get_unix_time() - start_time
+	
 	if game_time >= time_to_spawn_enemy:
 		attempt_spawn_enemy()
+		
 	time_to_spawn_enemy = game_time + time_between_enemy_spawns
 	pass
 
+# deal with enemy death by player
 func enemy_died(death_position, death_velocity):
 	spawn_orb(death_position, death_velocity)
 	number_of_enemies -= 1
 	
+# deal with enemy dying by contact with player
 func enemy_exploded():
 	number_of_enemies -= 1
 	
@@ -74,18 +85,26 @@ func player_hit_enemy(enemy):
 	
 	
 func _orb_entered_goal(orb, goal):
+	
+	# if it's the right goal for this orb
 	if orb.target_goal == goal.get_name():
+		# increase score
 		current_score += points_per_goal
+		# calculate current level based on score
 		current_level = floor(current_score/10) + 1
+		# increase enemy count in line with level increase
 		max_number_of_enemies = current_level
+		# update HUD
 		hud.set_current_score(current_score)
 		hud.set_level(current_level)
-	print(current_score)
+		
 	# remove this orb from the laser ignore array
 	laser_ignore_objects.remove(laser_ignore_objects.find(orb))
+	# remove orb from scene
 	orb.queue_free()
 	
 func _enemy_entered_goal(enemy):
+	# reset enemy position
 	enemy.set_pos(enemy_spawner.get_pos())
 	
 func _player_entered_goal(player):
@@ -102,10 +121,16 @@ func find_scene_node(name):
 	
 func attempt_spawn_enemy():
 	
-	var new_spawner_pos_x = rand_range(100,get_viewport().get_rect().size.x - 200)
-	var new_spawner_pos_y = enemy_spawner.get_pos().y
+	# create bounds so it doesn't spawn in walls
+	var spawner_min_x = 100
+	var spawner_max_x = get_viewport().get_rect().size.x - spawner_min_x
 	
+	# move spawner to new position
+	var new_spawner_pos_x = rand_range(spawner_min_x, spawner_max_x)
+	var new_spawner_pos_y = enemy_spawner.get_pos().y
 	enemy_spawner.set_pos(Vector2(new_spawner_pos_x, new_spawner_pos_y))
+	
+	# if there aren't enough enemies, spawn a new one
 	if number_of_enemies < max_number_of_enemies:
 		var new_enemy = enemy_spawner.spawn_enemy()
 		new_enemy.connect("entered_goal", self, "_enemy_entered_goal")
